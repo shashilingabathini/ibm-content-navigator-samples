ada581bdff9256b254e784f96a017d1443e8ba68
Emmanuel Shedu <Emmanuel.Shedu@ibm.com>
2021-03-12 18:42:46 -0800

    Update EditMessagePluginEventHandler.java

--- EditMessagePlugin/src/com/ibm/ecm/extension/editMessage/EditMessagePluginEventHandler.java
+++ EditMessagePlugin/src/com/ibm/ecm/extension/editMessage/EditMessagePluginEventHandler.java
@@ -10,30 +10,31 @@ import com.ibm.ecm.extension.PluginServiceCallbacks;
 public class EditMessagePluginEventHandler {
     /**
      * ActionMessage
-     *     private final int number;
-     *     private final String text;
-     *     private final String explanation;
-     *     private final String userResponse;
-     *     private final String adminResponse;
-     *     private final Exception exception;
-     *     private Map<String,String> message;
-     *     */
+     * Methods:
+     *     public int getNumber()
+     *     public String getText()
+     *     public String getExplanation()
+     *     public String getUserResponse()
+     *     public String getAdminResponse()
+     *     public Throwable getException()
+     *     public boolean hasChanged() - checks whether generateNewMessage() was called
+     *     public void generateNewMessage(int number, String text, String explanation, String userResponse, String adminResponse)
+     **/
+
+    //This method listens for error messages with specific key - add.item.error & message type - error
     public void editAddDocumentFailedMessage(@Observes @MessageKey("additem.error.failed") @MessageType("error") ActionMessage message, HttpServletRequest request, PluginServiceCallbacks callbacks) {
         String exceptionCause = message.getException().getMessage();
         String newText = message.getText() + " is caused by: " + exceptionCause;
         message.generateNewMessage(message.getNumber(), newText, message.getExplanation(), message.getUserResponse(), message.getAdminResponse());
-
     }
 
+    //This method listens for all error messages
     public void editErrorMessages(@Observes @MessageType("error") ActionMessage message, HttpServletRequest request, PluginServiceCallbacks callbacks) {
-//        String exceptionCause = message.getException().getMessage();
-//        String newText = message.getText() + " is caused by: " + exceptionCause;
-//        message.generateNewMessage(message.getNumber(), newText, message.getExplanation(), message.getUserResponse(), message.getAdminResponse());
 
     }
 
+    //This method listens for all messages
     public void editAllMessages(@Observes ActionMessage message, HttpServletRequest request, PluginServiceCallbacks callbacks) {
-//        message.generateNewMessage(message.getNumber(), message.getText(), message.getExplanation(), message.getUserResponse(), message.getAdminResponse());
 
     }
 
