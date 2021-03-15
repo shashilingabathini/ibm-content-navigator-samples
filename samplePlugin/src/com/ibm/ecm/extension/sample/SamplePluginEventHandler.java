b900e18d5ee4a8a87980a0775d44ed2bd82dc5c1
Emmanuel Shedu <Emmanuel.Shedu@ibm.com>
2021-03-15 08:45:00 -0700

    Move Edit Plugin Sample to main Sample Plugin

--- samplePlugin/src/com/ibm/ecm/extension/sample/SamplePluginEventHandler.java
+++ samplePlugin/src/com/ibm/ecm/extension/sample/SamplePluginEventHandler.java
@@ -7,6 +7,7 @@ import com.ibm.json.java.JSONArray;
 import com.ibm.json.java.JSONObject;
 
 import com.ibm.ecm.event.annotations.*;
+import com.ibm.ecm.event.messages.*;
 import com.ibm.ecm.extension.PluginServiceCallbacks;
 import com.ibm.ecm.json.JSONResponse;
 
@@ -46,5 +47,35 @@ public class SamplePluginEventHandler {
 
 	public void handleTopicMessageAsync(@ObservesAsync @Topic("SamplePlugin:samplePluginService") JSONResponse message) {
 		System.out.println("SamplePluginEventHandler received topic message: " + message);
+	}
+
+	/**
+	 * ActionMessage
+	 * Methods:
+	 *     public int getNumber()
+	 *     public String getText()
+	 *     public String getExplanation()
+	 *     public String getUserResponse()
+	 *     public String getAdminResponse()
+	 *     public Throwable getException()
+	 *     public boolean hasChanged() - checks whether generateNewMessage() was called
+	 *     public void generateNewMessage(int number, String text, String explanation, String userResponse, String adminResponse)
+	 **/
+
+	//This method listens for error messages with specific key - add.item.error & message type - error
+	public void editAddDocumentFailedMessage(@Observes @MessageKey("additem.error.failed") @MessageType("error") ActionMessage message, HttpServletRequest request, PluginServiceCallbacks callbacks) {
+		String exceptionCause = message.getException().getMessage();
+		String newText = message.getText() + " is caused by: " + exceptionCause;
+		message.generateNewMessage(message.getNumber(), newText, message.getExplanation(), message.getUserResponse(), message.getAdminResponse());
+	}
+
+	//This method listens for all error messages
+	public void editErrorMessages(@Observes @MessageType("error") ActionMessage message, HttpServletRequest request, PluginServiceCallbacks callbacks) {
+
+	}
+
+	//This method listens for all messages
+	public void editAllMessages(@Observes ActionMessage message, HttpServletRequest request, PluginServiceCallbacks callbacks) {
+
 	}
 }
